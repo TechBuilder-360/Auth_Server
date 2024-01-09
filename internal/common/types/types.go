ee2c1952e9ecae31fa7607d5ae956a31d7660fa4
Adegunwa Toluwalope <39342737+Toflex@users.noreply.github.com>
2024-01-09 23:30:57 +0100

    added user services (#6)
    Co-authored-by: Adegunwa Toluwalope <toluwalope.adegunwa@sterling.ng>

--- internal/common/types/types.go
+++ internal/common/types/types.go
@@ -1,7 +1,5 @@
 package types
 
-import "time"
-
 type (
 	LoginResponse struct {
 		Authentication Authentication `json:"authentication"`
@@ -10,17 +8,6 @@ type (
 
 	OrganisationMember struct {
 		ID string `json:"id"`
-	}
-
-	UserProfile struct {
-		ID            string    `json:"id"`
-		FirstName     string    `json:"first_name"`
-		LastName      string    `json:"last_name"`
-		DisplayName   string    `json:"display_name"`
-		EmailAddress  string    `json:"email_address"`
-		PhoneNumber   *string   `json:"phone_number"`
-		EmailVerified bool      `json:"email_verified"`
-		LastLogin     time.Time `json:"last_login"`
 	}
 
 	Authentication struct {
--- internal/common/types/user.go
+++ internal/common/types/user.go
@@ -1,28 +1,14 @@
 package types
 
-// AuthRequest ...
-type AuthRequest struct {
-	EmailAddress string `json:"email_address" validate:"required,email"`
-	Otp          string `json:"otp" validate:"required,len=6"`
-}
+import "time"
 
-// EmailRequest ...
-type EmailRequest struct {
-	EmailAddress string `json:"email_address" validate:"required,email"`
-}
-
-// Registration ...
-type Registration struct {
-	EmailAddress string  `json:"email_address" validate:"required,email"`
-	Avatar       *string `json:"avatar" validate:"url"`
-	FirstName    string  `json:"first_name" validate:"required"`
-	LastName     string  `json:"last_name" validate:"required"`
-	DisplayName  *string `json:"display_name"`
-	PhoneNumber  *string `json:"phone_number" validate:"e164"`
-}
-
-type UpgradeUserTierRequest struct {
-	IdentityNumber string `json:"identity_number" validate:"required" `
-	IdentityName   string `json:"identity_name" validate:"required"`
-	IdentityImage  string `json:"identity_image" validate:"required" `
+type UserProfile struct {
+	ID            string    `json:"id"`
+	FirstName     string    `json:"first_name"`
+	LastName      string    `json:"last_name"`
+	DisplayName   string    `json:"display_name"`
+	EmailAddress  string    `json:"email_address"`
+	PhoneNumber   *string   `json:"phone_number"`
+	EmailVerified bool      `json:"email_verified"`
+	LastLogin     time.Time `json:"last_login"`
 }
