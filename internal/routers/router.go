35b151fca1e79fb9c55473d360808fdcf1438148
Adegunwa Toluwalope <39342737+Toflex@users.noreply.github.com>
2024-02-27 20:53:07 +0100

    Fly (#12)
    * added user services (#6) (#7)
    
    Co-authored-by: Adegunwa Toluwalope <toluwalope.adegunwa@sterling.ng>
    
    * setting up fly deployment
    
    ---------
    
    Co-authored-by: Adegunwa Toluwalope <toluwalope.adegunwa@sterling.ng>

---
+++ internal/routers/router.go
@@ -0,0 +1,53 @@
+package routers
+
+import (
+	"github.com/TechBuilder-360/Auth_Server/internal/configs"
+	"github.com/TechBuilder-360/Auth_Server/internal/controllers"
+	"github.com/TechBuilder-360/Auth_Server/internal/middlewares"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/swagger"
+	log "github.com/sirupsen/logrus"
+)
+
+func SetupRoutes() *fiber.App {
+	router := fiber.New(fiber.Config{
+		CaseSensitive:         true,
+		ErrorHandler:          middlewares.DefaultErrorHandler,
+		DisableStartupMessage: true,
+	})
+
+	var (
+		authController  = controllers.DefaultAuthController()
+		usersController = controllers.DefaultUserController()
+		controller      = controllers.DefaultController()
+	)
+
+	//*******************************************
+	//******* Middlewares **********************
+	//*******************************************
+	router.Use(recover.New())
+
+	//*******************************************
+	//******* Controller **********************
+	//*******************************************
+	controller.RegisterRoutes(router)
+
+	//*******************************************
+	//******* Authentication **********************
+	//*******************************************
+	authController.RegisterRoutes(router)
+
+	//*************************************
+	//******* USERS **********************
+	//*************************************
+	usersController.RegisterRoutes(router)
+
+	if configs.IsSandBox() {
+		router.Get("/swagger/*", swagger.HandlerDefault)
+	}
+
+	log.Info("Routes have been initialized")
+
+	return router
+}
